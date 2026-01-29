f85fa928c0e2c776ebe4b6a43965c546783f7745
lincon <linconmatheuslm2012@gmail.com>
2026-01-29 09:59:07 -0300

    Add o  case 2 e atualiza que é o case 1, fazendo autenticação com o swagger no  Program.cs

--- piloto-inicial/Program.cs
+++ piloto-inicial/Program.cs
@@ -1,12 +1,12 @@
+UsuarioConfig.ExecutarCadastro();
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
@@ -33,6 +33,7 @@ app.MapGet("/weatherforecast", () =>
 })
 .WithName("GetWeatherForecast");
 
+UsuarioConfig.ExecutarCadastro();
 app.Run();
 
 record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
